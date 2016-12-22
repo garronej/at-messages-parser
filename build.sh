5fd5b080e5ae70a3f45e8e7c81d4a2fabc57e761
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2016-12-22 21:28:02 +0000

    no comment

--- build.sh
+++ build.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-echo $(pwd)
+node ./node_modules/typescript/bin/tsc --project ./ts/tsconfig.json
 
-#node ./node_modules/typescript/bin/tsc --project ./ts/tsconfig.json
-
-#node ./node_modules/jacob/cmd/cmd -t lexer.jacoblex -l ./generatedJs/Lexer.js -g parser.jacobgram -p ./generatedJs/Parser.js
+node ./node_modules/jacob/cmd/cmd -t lexer.jacoblex -l ./generatedJs/Lexer.js -g parser.jacobgram -p ./generatedJs/Parser.js
 
 
 
