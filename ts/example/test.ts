edf10ab623c83fa1d605859a6d4177e8143495a8
garronej <joseph.garrone@ensimag.grenoble-inp.fr>
2016-12-25 16:09:24 +0000

    fix error format

--- ts/example/test.ts
+++ ts/example/test.ts
@@ -8,11 +8,11 @@ let input=[
         'AT\r',
         'AT+CNUM=3,"SM"\r',
         '\r\n^BOOT:20952548,0,0,0,72\r\n',
-        '\r\nERROR+CNUM: "","+393701307294",145\r\n',
+        '\r\nERROR+CNUM: "","+393701307294",145\r\n\r\n',
         '\r\n^RSSI:99\r\n',
         '\r\n+CNUM: "CC","+8613987654321",129\r\n',
         '\r\n+WTF:ié"réflmmfe:eza&*\r\n',
-        '\r\nERROR+WTF:ié"réflmmfe:eza&*\r\n',
+        '\r\nERROR+WTF:ié"réflmmfe:eza&*\r\n\r\n',
         '\r\nOK\r\n'
 ].join("");
 
